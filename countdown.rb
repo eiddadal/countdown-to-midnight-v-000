f4e13543a67211f21407c45d903eccd72eb1be05
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-25 21:38:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,5 +8,10 @@ def countdown(number)
 end 
 "HAPPY NEW YEAR!"
 end
-def countdown_with_sleep
+def countdown_with_sleep(number)
+  number = 10 
+  while number > 0
+  puts "#{number} SECOND(S)!"
+   number -= 1
+end 
 end 
